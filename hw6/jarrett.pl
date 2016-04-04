f67f8395d7fb00bf872f4c62ca74776d84a26c0a
jtrain <jtrain184@gmail.com>
2016-04-04 00:52:01 -0500

    Modifying files

--- hw6/jarrett.pl
+++ hw6/jarrett.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl 
 #HW6- Perl script to read in user data and output to seperate files
 #CS390 Spring 2016
 #Philip Jarrett
@@ -7,7 +7,9 @@ use strict;
 use Data::Dumper;
 
 my @items;
-my @name;
+my $info = {};
+my($first,$last,$state);
+
 
 if($#ARGV+1 == 0)
 {
@@ -16,18 +18,54 @@ if($#ARGV+1 == 0)
 	exit 1;
 }
 
-my $info = {};
+open (OUT, ">./state.txt");
+open (OUT2, ">./dob.txt");
+open (OUT3, ">./salary.txt");
+
 while (<>) {
 	
     chomp;
-    my @items = split /:/, $_;
-     $info->{$items[1]} = { name => $items[0], 
-                            address => $items[2], 
-                            date => $items[3], 
-                            salary => $items[4] };
+	my @items = split /:/, $_;
+    my @name = $items[0];
+    my @phone = $items[1];
+    my @address = $items[2];
+    my @date = $items[3];
+    my @salary = $items[4];
+    #print @address;
+
+    #get states
+    for (@address){
+    	($state) = /([A-Z][A-Z])/;
+    }
+
+    #re-arrange names to last, first and print out with state
+    for (@name){
+    	($first,$last) = /(\w+)\s(\w+)/;
+    	
+    }
+
+    #print out names and DOB
+    for (@phone){
+    	my @name_DOB = "$last\t$first\t@date\n";
+    	my @name_state = "$state, $last, $first\n";
+    	my @sorted_name_state = sort { $a cmp $b} @name_state;
+    	print OUT @sorted_name_state;
+    	print OUT2 @name_DOB;
+    }
+  
+    #print out names and salary
+    for (@phone){
+    	my @name_salary = "$last\t$first\t@salary\n";
+    	print OUT3 @name_salary;
+    }
+   
 }
 
-print "$info{name}\n";
+
+    close(OUT);
+	close(OUT2);
+	close(OUT3);
+
 
 print "End of program!\n";
 
